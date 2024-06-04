b920d7604790be9a484673d516e29ab574a33249
Manan Rathod <76595511+MananRathod@users.noreply.github.com>
2024-06-05 02:34:14 +0530

    Create Binary_Trie.py

---
+++ Binary_Trie.py
@@ -0,0 +1,125 @@
+class BinaryTrieXor:
+    def __init__(self, max_num, num_cnt):  # bitwise xor
+        assert max_num >= 1
+        assert num_cnt >= 1
+        binary_state = 2
+        self.max_bit = max_num.bit_length() - 1
+        self.cnt_bit = num_cnt.bit_length()
+        self.node_cnt = (self.max_bit + 1) * num_cnt * binary_state
+        self.son_and_cnt = [0] * (self.node_cnt + 1)
+        self.ind = 1
+        self.mask = (1 << self.cnt_bit) - 1
+ 
+    def initial(self):
+        for i in range(self.node_cnt + 1):
+            self.son_and_cnt[i] = 0
+        self.ind = 1
+ 
+    def add(self, num: int, c=1) -> bool:
+        cur = 0
+        self.son_and_cnt[cur] += c
+        for k in range(self.max_bit, -1, -1):
+            bit = (num >> k) & 1
+            if not self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit:
+                self.son_and_cnt[(cur << 1) | bit] |= (self.ind << self.cnt_bit)
+                self.ind += 1
+            cur = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+            self.son_and_cnt[cur] += c
+        return True
+ 
+    def remove(self, num: int, c=1) -> bool:
+        if self.son_and_cnt[0] & self.mask < c:
+            return False
+        cur = 0
+        self.son_and_cnt[0] -= c
+        for k in range(self.max_bit, -1, -1):
+            bit = (num >> k) & 1
+            cur = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+            if cur == 0 or self.son_and_cnt[cur] & self.mask < c:
+                return False
+            self.son_and_cnt[cur] -= c
+        return True
+ 
+    def count(self, num: int):
+        cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (num >> k) & 1
+            cur = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+            if cur == 0 or self.son_and_cnt[cur] & self.mask == 0:
+                return 0
+        return self.son_and_cnt[cur] & self.mask
+ 
+    def get_maximum_xor(self, x: int) -> int:
+        """get maximum result for constant x ^ element in array"""
+        if self.son_and_cnt[0] & self.mask == 0:
+            return 0
+        res = 0
+        cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (x >> k) & 1
+            nxt = self.son_and_cnt[(cur << 1) | (bit ^ 1)] >> self.cnt_bit
+            if nxt == 0 or self.son_and_cnt[nxt] & self.mask == 0:
+                cur = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+            else:
+                res |= 1 << k
+                cur = nxt
+        return res
+ 
+    def get_minimum_xor(self, x: int) -> int:
+        """get minimum result for constant x ^ element in array"""
+        if self.son_and_cnt[0] & self.mask == 0:
+            return inf
+        res = 0
+        cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (x >> k) & 1
+            nxt = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+            if nxt == 0 or self.son_and_cnt[nxt] & self.mask == 0:
+                res |= 1 << k
+                cur = self.son_and_cnt[(cur << 1) | (bit ^ 1)] >> self.cnt_bit
+            else:
+                cur = nxt
+        return res
+ 
+    def get_kth_maximum_xor(self, x: int, rk) -> int:
+        """get kth maximum result for constant x ^ element in array"""
+        assert rk >= 1
+        if self.son_and_cnt[0] & self.mask < rk:
+            return -inf
+        res = 0
+        cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (x >> k) & 1
+            nxt = self.son_and_cnt[(cur << 1) | (bit ^ 1)] >> self.cnt_bit
+            if nxt == 0 or self.son_and_cnt[nxt] & self.mask < rk:
+                if nxt:
+                    rk -= self.son_and_cnt[nxt] & self.mask
+                cur = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+            else:
+                res |= 1 << k
+                cur = nxt
+        return res
+ 
+    def get_cnt_smaller_xor(self, x: int, y: int) -> int:
+        """get cnt result for constant x ^ element <= y in array"""
+        if self.son_and_cnt[0] & self.mask == 0:
+            return 0
+        res = 0
+        cur = 0
+        for k in range(self.max_bit, -1, -1):
+            bit = (x >> k) & 1
+            if not (y >> k) & 1:
+                nxt = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+                if nxt == 0 or self.son_and_cnt[nxt] & self.mask == 0:
+                    return res
+                cur = nxt
+            else:
+                nxt = self.son_and_cnt[(cur << 1) | bit] >> self.cnt_bit
+                if nxt:
+                    res += self.son_and_cnt[nxt] & self.mask
+                nxt = self.son_and_cnt[(cur << 1) | (bit ^ 1)] >> self.cnt_bit
+                if nxt == 0 or self.son_and_cnt[nxt] & self.mask == 0:
+                    return res
+                cur = nxt
+        res += self.son_and_cnt[cur] & self.mask
+        return res
